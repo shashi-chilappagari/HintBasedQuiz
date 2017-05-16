9a7d0c3d199a086c8732ba313e29612668ea89f8
Shashi Kiran Chilappagari <shashickiran@gmail.com>
2017-05-16 11:57:24 -0700

    Improved logic in the .js file
    Removed hard codings and improved overall flow.

--- question.js
+++ question.js
@@ -1,41 +1,40 @@
 var questions=[
 	{
-		"question": "Which element is present in doodoo",
-		"answer_images":[],
-		"options":["Sodium","Potassium","Calcium","Hydrogen"],
-		"answer": "Calcium",
-		"hints":["I am yellow in color","Daddy buys me every Thursday"]
-	},
-	{
 		"question": "My heart rate is 10 beats per minutes and can be heard from a distance of 1 mile.",
-		"question_image":[],
-		"answer_images":["Whale.jpg","Dolphin.jpg","SeaLion.jpg","Shark.jpg"],
+		"questionType": ["MultChoice"],
 		"options":["Blue Whale","Dolphin","Sea Lion","Shark"],
-		"answer": "Blue Whale",
-		"hints":["I evolved from Indohyus","The Hippopotamus is my closest relative","I am the largest mammal in the world"]
+		"answer": ["Blue Whale"],
+		"response":["Good job. Blue whale is the correct answer","Sorry, the correct answer is Blue Whale"],
+		"questionImage":[],
+		"answerImages":["Whale.jpg","Dolphin.jpg","SeaLion.jpg","Shark.jpg"],
+		"hints":["I evolved from Indohyus","The Hippopotamus is my closest relative","I am the largest mammal in the world"],
+		"audioClips":['Whale.mp3'],
+		"audioIntro":['Want to know more about the strange origin of Whales. Listen to this audio clip.'],
+		"feedbackType":["Correct/Incorrect"],
+		"feedback":["Blue whales are amazing animals","Dolphins, sea lions and sharks are not so amazing"]
 	},
 	{
 		"question": "I won the Nobel Prize in Physics for Photo-Electric Effect.",
-		"answer_images":["Einstein.jpg","Max_Planck.jpg","Niels_Bohr.jpg","Schrodinger.jpg"],
+		"questionType": ["MultChoice"],
 		"options":["Albert Einstein","Max Planck","Niels Bohr","Erwin Schrodinger"],
-		"answer": "Albert Einstein",
-		"hints":["I immigrated to USA and worked at Princeton","I am most famous for my work on Relativity"]
-
-	},
-	{
-		"question": "Who is the best chikku baby?",
-		"answer_images":["Dyuthi.jpg","Dithya.jpg","Maanvi.jpg","Snithika.jpg"],
-		"options":["Dyuthi","Dithya","Maanvi","Snithika"],
-		"answer": "Dyuthi",
-		"hints":["I know a lot of elements","I am very good at additions"]
+		"answer": ["Albert Einstein"],
+		"response":["Good job. Albert Einstein is the correct answer","Sorry, the correct answer is Albert Einstein"],
+		"questionImage":[],
+		"answerImages":["Einstein.jpg","Max_Planck.jpg","Niels_Bohr.jpg","Schrodinger.jpg"],
+		"hints":["I immigrated to USA and worked at Princeton","I am most famous for my work on Relativity"],
+		"audioIntro":['Here is some information on how rainbows are formed.'],
+		"audioClips":["Rainbows.mp3"],
+		"feedbackType":["OptionBased"],
+		"feedback":["Einstein did not win for his most famous work","Max Plack won for his contributions to QUnatum mechanics",
+								"Neils Bohr won for atomic model","Schrodinger won for his service to cats"]
 	},
 	{
 		"question": "Potassium is present in which fruit",
-		"answer_images":["Banana.jpg","Mango.jpg","Strawberry.jpg","Pineapple.jpg"],
+		"questionType": ["MultChoice"],
 		"options":["Banana","Mango","Strawberry","Pineapple"],
-		"answer": "Banana",
-		"hints":["I am yellow in color","Daddy buys me every Thursday"]
+		"answer": ["Banana"],
+		"response":["It is Banana indeed","Sorry, the fruit that contains Potassium is Banana"],
+		"answerImages":["Banana.jpg","Mango.jpg","Strawberry.jpg","Pineapple.jpg"],
+		"hints":["I am yellow in color","I may probbaly be the most common fruit"]
 	}
-
-
 ];
--- quiz-script.js
+++ quiz-script.js
@@ -1,57 +1,99 @@
 var currentQuestion						=0;
 var score											=0;
 var totalQuestions						=questions.length;
-var optionOrder								=[0,1,2,3];
+var optionOrder								=[2,0,1,3];
 var numHintsCurrentQuestion		=0;
 var currentHintNumber					=0;
-
 var container									=document.getElementById('quizContainer');
 var questionContainer					=document.getElementById('questionContainer');
 var feedbackContainer					=document.getElementById('feedbackContainer');
 var container									=document.getElementById('quizContainer');
-
 var nextButton								=document.getElementById('nextButton');
 var submitButton							=document.getElementById('submitButton');
 var hintButton								=document.getElementById('hintButton');
-
-var resultCont								=document.getElementById('result');
+var resultContainer						=document.getElementById('resultContainer');
 var feedback									=document.getElementById('feedback');
 
+function showAudioClips(){
+	var para=document.createElement("p");
+	var text='Here is a clip on Rainbows and how they are formed.'
+	var node=document.createTextNode(text);
+	para.appendChild(node);
+	resultContainer.appendChild(para);
+	resultContainer.innerHTML+= '<br> <audio controls><source src="audio/Rainbows.mp3" type="audio/mpeg">'+
+	'Your browser does not support the audio element.</audio>';
+	var para=document.createElement("p");
+	var text='Here is a clip on Whales and how they evolved.'
+	var node=document.createTextNode(text);
+	para.appendChild(node);
+	resultContainer.appendChild(para);
+	resultContainer.innerHTML+= '<br> <audio controls><source src="audio/Whale.mp3" type="audio/mpeg">'+
+	'Your browser does not support the audio element.</audio>';
+}
 function loadQuestion(questionIndex,optionOrder){
-	var questionEl=document.getElementById('question');
+	/*Restting the hint number to 0*/
 	currentHintNumber=0;
+	/*Making question container visible and hiding feedback container*/
 	questionContainer.style.display='';
 	feedbackContainer.style.display='none';
-
+	/*Loading the question*/
 	var q=questions[questionIndex];
+	var questionEl=document.getElementById('question');
 	questionEl.textContent=q.question;
-
-	var optionTable=document.getElementById("optionTable");
-	if(optionTable){
-			var numRows=optionTable.rows.length;
-			for (var i=0;i<numRows;i++){
-				optionTable.deleteRow(-1);
-			}
+	/*Loading the question image if present*/
+	var questionImage=document.getElementById("questionImage");
+	var questionImageTable=document.getElementById("questionImageTable");
+	questionImageTable.innerHTML='';
+	var questionImageRow;
+	/*Creating a new image table for current question, if present*/
+	if(q.questionImage){
+		if(q.questionImage.length>0){
+			questionImage.style.display='';
 		}
-		var imageRow;
-		var textRow;
-		if(q.answer_images.length>0){
-			imageRow=optionTable.insertRow(-1);
+		else{
+			questionImage.style.display='none';
 		}
-		if(q.options.length>0){
-			textRow=optionTable.insertRow(-1);
+		questionImageRow=questionImageTable.insertRow(-1);
+		for (var i=0;i<q.questionImage.length;i++){
+			var questionImageCol=questionImageRow.insertCell(i);
+			questionImageCol.innerHTML='<img  class="questionImage" src=img/'+q.questionImage[i]+'>';
 		}
+	}
+	else{
+		questionImage.style.display='none';
+	}
+	/*Displaying answer options with images, if present
+	First, the old table is deleted. Then new rows are added based on the question*/
+	var optionTable=document.getElementById('optionTable');
+	var imageRow;
+	var textRow;
 	var imgsrc, imgid, imgcol, textCol;
-	for (var i=0;i<q.answer_images.length;i++){
-		imgsrc="img/"+q.answer_images[optionOrder[i]];
+	optionTable.innerHTML='';
+	if(q.answerImages.length>0){
+		imageRow=optionTable.insertRow(-1);
+	}
+	if(q.options.length>0){
+		textRow=optionTable.insertRow(-1);
+	}
+	for (var i=0;i<q.answerImages.length;i++){
+		imgsrc="img/"+q.answerImages[optionOrder[i]];
 		imgid="img"+(i+1);
 		imgcol=imageRow.insertCell(i);
-		imgcol.innerHTML='<img + id='+imgid+' class="image" src='+imgsrc+'>';
+		imgcol.innerHTML='<img  id='+imgid+' class="image" src='+imgsrc+'>';
 	}
 	for (var i=0;i<q.options.length;i++){
 		textCol=textRow.insertCell(i);
-		textCol.innerHTML='<input type="radio" name="option"'+' value='+ (i+1)+ ' />'+q.options[optionOrder[i]];
+		if(q.questionType){
+			if(q.questionType=='True/False'){
+				textCol.innerHTML='<input type="radio" id="option'+(i+1)+'" name="option"'+' value='+ (i+1)+ ' />'+q.options[optionOrder[i]];
+			}
+			else {
+				textCol.innerHTML='<input type="checkbox" id="option'+(i+1)+'" name="option"'+' value='+ (i+1)+ ' />'+q.options[optionOrder[i]];
+			}
+		}
 	}
+	/* Displaying the hint button if hints are present for the current question.
+	Deleting the previous hints*/
 	numHintsCurrentQuestion=q.hints.length;
 	if(numHintsCurrentQuestion>0){
 		hintButton.style.display='';
@@ -61,71 +103,110 @@ function loadQuestion(questionIndex,optionOrder){
 	}
 	var hintBox=document.getElementById('hintBox');
 	hintBox.style.display='none';
-	var hintTable=document.getElementById("hintTable");
-	if(hintTable){
-			var numRows=hintTable.rows.length;
-			for (var i=0;i<numRows;i++){
-				hintTable.deleteRow(-1);
-			}
-		}
+	hintBox.innerHTML='';
 };
-
 function provideHint(){
 	var q=questions[currentQuestion];
 	var hintBox=document.getElementById('hintBox');
 	hintBox.style.display='';
-	var hintTable=document.getElementById("hintTable");
-	var newRow=hintTable.insertRow(currentHintNumber);
-	newRow.innerHTML=q.hints[currentHintNumber];
+	insertText(q.hints[currentHintNumber],'hintBox');
 	if(currentHintNumber==numHintsCurrentQuestion-1){
 		hintButton.style.display='none';
 	}
 	currentHintNumber++;
 }
+function insertText(text,parentNode){
+	var parent=document.getElementById(parentNode);
+	var para=document.createElement("p");
+	var node=document.createTextNode(text);
+	para.appendChild(node);
+	parent.appendChild(para);
+}
 function provideFeedback(){
-
-	var selectedOption=document.querySelector('input[type=radio]:checked');
-	if(!selectedOption){
+	var checkedAnswers=[];
+	var j=0;
+	var id='';
+	for(var i=0;i<questions[currentQuestion].options.length;i++){
+		id='option'+(i+1);
+		if(document.getElementById(id).checked){
+			checkedAnswers[j]=i;
+			j++;
+		}
+	}
+	if(j==0){
 		alert('Please select your answer!');
 		return;
 	}
 	questionContainer.style.display='none';
 	feedbackContainer.style.display='';
 	hintButton.style.display='none';
-	var answerIndex=selectedOption.value;
-	if(questions[currentQuestion].answer==questions[currentQuestion].options[optionOrder[answerIndex-1]]){
+	var answerIndex=-1;
+	var match=0;
+	for(var index=0;index<checkedAnswers.length;index++){
+		answerIndex=checkedAnswers[index];
+		for(var i=0;i<questions[currentQuestion].answer.length;i++){
+			if(questions[currentQuestion].answer[i]==questions[currentQuestion].options[optionOrder[answerIndex]]){
+				match++;
+			}
+		}
+	}
+	var correct=false;
+	feedbackContainer.style.display='';
+	if((match==checkedAnswers.length)&&(match==questions[currentQuestion].answer.length)){
+		correct=true;
 		score+=5;
-		feedback.style.display='';
-		feedback.style.background="lightgreen";
-		feedback.innerHTML="Correct answer";
+		feedbackContainer.style.background="darkseagreen";
+		insertText(questions[currentQuestion].response[0],'feedbackContainer');
 	}
 	else{
-		feedback.style.display='';
-		feedback.style.background="crimson";
-		feedback.innerHTML="Incorrect answer";
+		insertText(questions[currentQuestion].response[1],'feedbackContainer');
+		feedbackContainer.style.background="darksalmon";
+	}
+		if(questions[currentQuestion].feedback){
+			if(questions[currentQuestion].feedbackType=='Correct/Incorrect')
+			{
+				if(correct){
+					insertText(questions[currentQuestion].feedback[0],'feedbackContainer');
+					}
+				else {
+					insertText(questions[currentQuestion].feedback[1],'feedbackContainer');
+					}
+			}
+			if(questions[currentQuestion].feedbackType=='OptionBased'){
+				insertText(questions[currentQuestion].feedback[optionOrder[checkedAnswers[0]]],'feedbackContainer');
+				}
+			}
+
+	if(questions[currentQuestion].audioIntro){
+		insertText(questions[currentQuestion].audioIntro,'feedbackContainer');
+		feedbackContainer.innerHTML+= '<audio id="audio" controls><source src="audio/'+ questions[currentQuestion].audioClips
+		+'" type="audio/mpeg" id="audioClip">'+
+		'Your browser does not support the audio element.</audio>';
 	}
 	nextButton.style.display='';
 	submitButton.style.display='none';
 }
 
 function loadNextQuestion(){
-	var selectedOption=document.querySelector('input[type=radio]:checked');
-	selectedOption.checked=false;
-	feedback.style.display="none";
+	feedbackContainer.style.display="none";
+	feedbackContainer.innerHTML='';
 	currentQuestion++;
 	if(currentQuestion==totalQuestions-1){
 		nextButton.textContent='Finish';
 	}
 	if(currentQuestion==totalQuestions){
-		container.style.display='none';
-		resultCont.style.display='';
-		resultCont.textContent='Your score: '+ score;
+		questionContainer.style.display='none';
+		feedbackContainer.style.display='none';
+		nextButton.style.display='none';
+		resultContainer.style.display='';
+		var text='Your score: '+ score;
+	  insertText(text,'resultContainer');
+		showAudioClips();
 		return;
 	}
 	loadQuestion(currentQuestion,optionOrder);
 	nextButton.style.display='none';
 	submitButton.style.display='';
-
 };
 
 loadQuestion(currentQuestion,optionOrder);
